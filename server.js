8bd80199f5f3a9209c50cef38db29316184670dd
65ani <anitha.nhk26@gmail.com>
2024-09-19 07:42:59 +0000

    Adding create,get,getbooksid,update,delete

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const bookRoutes = require('./routes/bookRoutes');
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +14,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("HomePage Of The App");
 });
+
+app.use('/api', bookRoutes); // Use book routes with prefix '/api'
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
